2574729766be0be7ad3f300d6c0f295a7b6b7931
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2023-01-30 13:21:40 -0500

    added options

--- main.py
+++ main.py
@@ -29,18 +29,21 @@ class Library:
 
     def list_books(self):
         print("List of books in the library:")
-        for book in self.books:
-            print(f"{book.title} by {book.author}")
+        if len(self.books) > 0:
+            for book in self.books:
+                print({book.title}+"by ping pong"+{book.author})
+        else:
+            print("Library is Empty!")
 
 # create library
 library = Library()
 
 print("Hello, Welcome to the most efficient Library Management System!")
-print("You can do the following: \n 1. List of Books Available \n 2. Add A book \n 3. Add a book")
+print("You can do the following: \n 1. List of Books Available \n 2. Add A book \n 3 Borrow a book \n 4. Return a book")
 anss = "Y"
 while True:    
     if anss in "yY":
-        ans = input("enter what you want to do")
+        ans = int(input("enter what you want to do"))
 
         # list books in library
         if ans == 1:
@@ -55,17 +58,16 @@ while True:
             library.add_book(book1)
         elif ans == 3:
             # borrow books
+            btitle = input("enter title of book to be borrowed")
+            bauthor = input("enter author")
             book1.borrow_book()
+        elif ans == 4:
+            # return books
+            btitle = input("enter title of book to be returned")
+            bauthor = input("enter author")
+            book1.return_book()
     else:
         break
     anss= input("do you want to continue? enter Y for yes, N for no")
 
-
-# try to borrow already borrowed book
-book2.borrow_book()
-
-
-# return books
-book1.return_book()
-book2.return_book()
 
